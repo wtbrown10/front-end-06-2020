9f73e2927ab882a5d23387c80b0942c15a9dc675
Will Brown <wtbrown10@gmail.com>
2020-06-02 18:27:48 -0400

    dropdown2

--- hotelflex.js
+++ hotelflex.js
@@ -1,5 +1,6 @@
 let hotelRooms = [[101, 102, 103, 104], [201, 202, 203, 204], [301, 302, 303]]
 let roomTypes = ['Queen', 'King', 'Twin']
+let queenRooms = [101, 102, 103, 104]
 
 let dropDown = "<select>"
 
@@ -12,3 +13,15 @@ for (let i = 0; i < hotelRooms.length; i++) {
 dropDown = dropDown + "</select>"
 
 document.getElementById('dropDown').innerHTML = dropDown
+
+let dropDown2 = "<select>"
+
+for (let i = 0; i < queenRooms.length; i++) {
+    // for (let j = 0; j < hotelRooms[i].length; j++) {
+    dropDown2 = dropDown2 + `<option value='${queenRooms[i]}'>${queenRooms[i]}</option>`
+}
+
+// }
+dropDown2 = dropDown2 + "</select>"
+
+document.getElementById('dropDown2').innerHTML = dropDown2
